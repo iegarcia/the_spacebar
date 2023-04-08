e7cf2a824d87a1cabe049e7071325dc371d9e746
Ignacio Garcia <nachogarcia003@gmail.com>
2023-04-08 11:30:01 -0300

    services

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,15 +2,23 @@
 
 namespace App\Controller;
 
-use Michelf\MarkdownInterface;
+use App\Service\MarkdownHelper;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Cache\Adapter\AdapterInterface;
+
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
+
+    private $isDebug;
+
+    public function __construct(bool $isDebug)
+    {
+        $this->isDebug = $isDebug;
+    }
     /**
      * @Route("/", name="article_home")
      */
@@ -22,10 +30,9 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache): Response
+    public function show($slug, MarkdownHelper $markdownHelper): Response
     {
-        dump($markdown);
-        die;
+
         $comments = [
             "test 1",
             "test 2",
@@ -51,15 +58,7 @@ cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim ca
 fugiat.
 EOF;
 
-
-
-        $item = $cache->getItem('markdown_' . md5($articleContent));
-        if (!$item->isHit()) {
-            $item->set($markdown->transform($articleContent));
-            $cache->save($item);
-        }
-
-        $articleContent = $item->get();
+        $articleContent = $markdownHelper->parse($articleContent);
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace("-", " ", $slug)),
